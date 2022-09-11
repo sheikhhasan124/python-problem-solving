fb56171a1fd647381cc15cf82c1017b4787cc0ac
hasan-ullah <mdhasanullah124@gmail.com>
2022-09-11 22:20:57 +0600

    py date done

--- basic-part-one.py
+++ basic-part-one.py
@@ -1,4 +1,10 @@
 """ print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!") """
 
 
-print("aaaaaaaa\nbbbbbbbbb\n\tccccccc\n\t\tdddddddd")
+# problem-1 -----------
+# print("aaaaaaaa\nbbbbbbbbb\n\tccccccc\n\t\tdddddddd")
+
+# problem-2 -----------
+""" import datetime
+time = datetime.datetime.now()
+print("date is",time) """
